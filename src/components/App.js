fd97ef38e986b11dcb319dc91d629f8825ea8c3e
miladpeagh <miladpeagh1999@gmail.com>
2021-10-04 22:30:16 +0330

    1-refactore app 2-make header initial

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/App.css';
+import Header from './Header';
+
+
+class App extends React.Component {
+
+
+  render(){
+    return(
+        <>
+          <Header/>
+        </>
+      );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import '../css/App.css';
+
+class Header extends React.Component {
+	state = {
+		weekDay: '',
+		mountDay:'',
+		mount: '',
+		year: ''
+	};
+	weekDay = new Array(7);
+
+	monthNames = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	];
+
+	componentDidMount(){
+		const date = new Date();
+		this.weekDay[0] = "Sunday";
+		this.weekDay[1] = "Monday";
+	    this.weekDay[2] = "Tuesday";
+ 		this.weekDay[3] = "Wednesday";
+		this.weekDay[4] = "Thursday";
+		this.weekDay[5] = "Friday";
+		this.weekDay[6] = "Saturday";
+    	this.setState({
+    		weekDay : this.weekDay[date.getDay()],
+    		mountDay: date.getDate(),
+    		mount: this.monthNames[date.getMonth()],
+    		year: date.getFullYear()
+    	});
+  	}
+
+  render(){
+    return(
+        <>
+         <div className="header-element">
+         	<div className="ui container">
+         		<div className="row">
+         			<div className="col-md-12">
+	         			<div>
+	         				<div className="header-button-left">
+	         					<button className="ui inverted button">Today</button>
+	         				</div>
+	         				<div className="header-button-right">
+	         					<button className="ui inverted button">Calendar</button>
+	         				</div>
+	         			</div>
+	         		</div>
+	         		<div className="col-md-12" style={{ paddingTop:'5%'}}>
+	         				<h1>
+		         				{ this.state.weekDay }, {this.state.mountDay}th
+	         				</h1>
+	         				<h1>
+	         					{ this.state.mount } { this.state.year }
+	         				</h1>
+	         		</div>
+         		</div>
+         	</div>
+         </div>
+
+        </>
+      );
+  }
+}
+
+export default Header;
